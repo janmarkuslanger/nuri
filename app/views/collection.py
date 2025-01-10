65f38a307df91d1bee75e994150415e464b803c1
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2025-01-10 12:47:48 +0000

    add edit route

--- app/views/collection.py
+++ app/views/collection.py
@@ -10,7 +10,7 @@ def index():
     return render_template("list_collections.html", collections=collections)
 
 @view.route("/create", methods=["GET", "POST"])
-def create_collection():
+def create():
     if request.method == "POST":
         name = request.form.get("name")
         alias = request.form.get("alias")
@@ -27,3 +27,29 @@ def create_collection():
             return redirect(url_for("collection.index"))
         
     return render_template("create_collection.html")
+
+@view.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    collection = Collection.query.get_or_404(id)
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        alias = request.form.get("alias")
+        description = request.form.get("description")
+
+        existing_alias = Collection.query.filter(Collection.alias == alias, Collection.id != id).first()
+        if existing_alias:
+            flash("Alias already exists", "error")
+            return redirect(url_for("collection.edit", id=id))
+
+        if name and alias:
+            collection.name = name
+            collection.alias = alias
+            collection.description = description
+            db.session.commit()
+            flash("Collection updated successfully!", "success")
+            return redirect(url_for("collection.index"))
+
+    return render_template("edit_collection.html", collection=collection)
+
+
